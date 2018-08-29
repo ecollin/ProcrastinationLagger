b0ae584fbbb396104552b91473bb6fd910687664
Enrique <enriquecollin99@gmail.com>
2018-08-29 01:13:59 -0400

    Modularized adding site to make it easier to write chrome.storage code

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
-//adds the website in the text field to the list of sites to delay loading of
-//empties the text field
-function addSite() {
-  let inputField = document.querySelector("#add-input");
-  if (!inputField.value) return; //do nothing on empty field 
+//Adds the given host to the list of sites to lag when loading
+//with a button to remove it
+function addSite(host) {
   let list = document.getElementsByTagName("ul")[0];
   let newLi = document.createElement("li"); //list item of added site
-  newLi.innerHTML = inputField.value;
+  newLi.innerHTML = host;
   let rmButton = document.createElement("button"); //button to remove site
   rmButton.addEventListener("click", () => {
     newLi.remove(); 
@@ -14,9 +12,20 @@ function addSite() {
   rmButton.class = "rmButton";
   newLi.appendChild(rmButton);
   list.appendChild(newLi);
+}
+
+//If the input box is not empty, adds the hostname in it to the list of sites.
+function addInputToSite() {
+  let inputField = document.querySelector("#add-input");
+  if (!inputField.value) return; //do nothing on empty field if e is true 
+  addSite(inputField.value);
 
   inputField.value = ""; //clear input field
 }
+chrome.storage.local.get(["addedSites"], result => {
+  
+});
+
 
 let slider = document.querySelector("#slider"); 
 slider.addEventListener("input", () => {
@@ -24,10 +33,10 @@ slider.addEventListener("input", () => {
   sliderDisplay.innerHTML = slider.value;
 });
 
-document.querySelector("button").addEventListener("click", addSite);
+document.querySelector("button").addEventListener("click", addInputToSite); 
 document.querySelector("#add-input").addEventListener("keyup", (event) => {
   if (event.keyCode == 13) { //if user presses enter on input
-    addSite();
+    addInputToSite(); 
   }
 });
 
