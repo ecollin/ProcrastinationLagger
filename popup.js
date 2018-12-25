ff4754e2dabccdaf4cdab5cd298605960553a35d
Enrique <enriquecollin99@gmail.com>
2018-12-25 15:31:55 -0500

    Touched up var names and id's in popup.js and .html. Started adding timestamp functionality

--- popup.js
+++ popup.js
@@ -1,22 +1,49 @@
-//Adds the given host to the list of sites to lag when loading
-//Also adds a button to remove it from the list 
-function addSite(host) {
+/*
+ * The following class is used to store the websites the user wishes to delay
+ * the loading of when he visits and to store the last time he visited them.
+ * Time is tracked so if the user wishes he can prevent the load delay
+ * if he already sat through it say 30 seconds ago, or however long he chooses.
+ */
+class DelayedSite {
+  constructor(domain) {
+    this.lastVisit = 0; /* Stores the timestamp of last visit in seconds. */
+    this.site = domain; /* The name of the site to delay */
+  }
+  /*
+   * Updates the lastVisit property of the object to hold the timestamp 
+   * in seconds of the last time the site was visited. 
+   */
+  function visit() {
+    this.lastVisit = Date.now(); 
+  }
+}
+
+
+/* 
+ * Adds the given host to the list of sites to lag when loading
+ * Also adds a button to remove it from the list 
+ */
+function addSite(domain) {
   let list = document.getElementsByTagName("ul")[0];
-  let newLi = document.createElement("li"); //list item of added site
-  newLi.innerHTML = host;
-  let rmButton = document.createElement("button"); //button to remove site
-  const indexOfHost = list.length - 1;
+  let newLi = document.createElement("li"); /* new list item for added site */
+  newLi.innerHTML = domain;
+  let rmButton = document.createElement("button"); /* button to remove site */
+  const indexOfDomain = list.length - 1;
   rmButton.addEventListener("click", () => {
     newLi.remove(); //remove site from list 
-    chrome.storage.sync.get({"addedSites":[]}, (result) => {
-    //result is obj with addedSites field that is stored val or a default []
-    //remove site from storage
+    chrome.storage.local.get({"addedSites":[]}, (result) => {
+      if (chrome.runtime.lastError) {
+        console.log("Error in chrome.storage.local.get!");
+        window.close();
+      }
+    //result is obj with addedSites field that is the stored val or a default []
+    //Next lines remove the site from storage
       let sitesArr = result.addedSites;
-      sitesArr.splice(indexOfHost,1); //removes indexOfHost from sitesArr
-      chrome.storage.sync.set({"addedSites": sitesArr}); 
+      sitesArr.splice(indexOfDomain,1); //removes indexOfDomain from sitesArr
+      chrome.storage.local.set({"addedSites": sitesArr}); 
       
       let addedSitesDisplay = document.querySelector("#added-sites");
-      if (sitesArr.length == 0) { //if only elt was removed, don't display list
+      if (sitesArr.length == 0) { //don't display list if no elements left
         addedSitesDisplay.style.display = "none"; 
       }
 
@@ -27,8 +54,11 @@ function addSite(host) {
   newLi.appendChild(rmButton);
   list.appendChild(newLi);
 }
-//If the input form is not empty, adds the hostname it contains
-//to the list of added sites. 
+
+/*
+ * If the popup's input form is not empty, adds the hostname it contains
+ * to the list of added sites. 
+ */
 function addInputToSite() {
   let addedSitesDisplay = document.querySelector("#added-sites");
   if (addedSitesDisplay.style.display == "none") {
@@ -39,38 +69,35 @@ function addInputToSite() {
   inputField.value = ""; 
   if (!newSite) return;   
   addSite(newSite);
-  chrome.storage.sync.get({"addedSites":[]}, (result) => {
+  chrome.storage.local.get({"addedSites":[]}, (result) => {
     if (chrome.runtime.lastError) {
-      console.log("Error in chrome.storage.sync.get!");
+      console.log("Error in chrome.storage.local.get!");
       window.close();
     }
     let sitesArr = result.addedSites;
     sitesArr.push(newSite); //add one more site to stored map 
-    chrome.storage.sync.set({"addedSites": sitesArr}); 
+    chrome.storage.local.set({"addedSites": sitesArr}); 
   });
 }
 
-//sets up the event handlers for the slider and the add site button.
+/*
+ * sets up the event handlers for the slider and the add site button.
+ */
 function setupHandlers() {
-  let slider = document.querySelector("#slider"); 
-  slider.addEventListener("input", () => {
-    let sliderDisplay = document.querySelector("#slider-val");
-    sliderDisplay.innerHTML = slider.value;
-    chrome.storage.sync.set({"sliderVal":slider.value});
-  });
-    
-  document.querySelector("button").addEventListener("click", addInputToSite); 
-  document.querySelector("#add-input").addEventListener("keyup", (event) => {
+  let slider = document.querySelector("add-input").
+  addEventListener("keyup", (event) => {
     if (event.keyCode == 13) { //if user presses enter on input
       addInputToSite(); 
     }
   });
 }
 
-//on load up want to run the following code to set things up
-chrome.storage.sync.get({"addedSites":[], "sliderVal": 5}, (result) => {
+/*
+ * On load up want to run the following code to set things up visually in popup
+ */
+chrome.storage.local.get({"addedSites":[], "sliderVal": 5}, (result) => {
   if (chrome.runtime.lastError) {
-    console.log("Error in chrome.storage.sync.get!");
+    console.log("Error in chrome.storage.local.get!");
     window.close();
   }
   let sitesArr = result.addedSites; 
@@ -82,7 +109,6 @@ chrome.storage.sync.get({"addedSites":[], "sliderVal": 5}, (result) => {
   }
   let sliderDisplay = document.querySelector("#slider-val");
   sliderDisplay.innerHTML = result.sliderVal; //get secs of delay from storage
-
   setupHandlers();  
 });
 
